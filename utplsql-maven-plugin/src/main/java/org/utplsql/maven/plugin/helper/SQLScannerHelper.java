2a87b81596b692b8191a5be6ecc88a7a889bd88c
Jerome Garec <jgarec@users.noreply.github.com>
2018-07-28 09:56:48 +0200

    Implements #4 :
    * Directory or includes are now optional (when the other one is set)
    * Add new tests
    * Fix a bug where files paths where relative to the plugin and not the
    tested project
    * Fix previous tests

--- utplsql-maven-plugin/src/main/java/org/utplsql/maven/plugin/helper/SQLScannerHelper.java
+++ utplsql-maven-plugin/src/main/java/org/utplsql/maven/plugin/helper/SQLScannerHelper.java
@@ -27,12 +27,21 @@ public class SQLScannerHelper {
      * @param resouces
      * @return
      */
-    public static List<String> findSQLs(File baseDir,List<Resource> resources) {
+    public static List<String> findSQLs(File baseDir,List<Resource> resources, String defaultDirectory, String defaultFilePattern) {
         List<String> founds = new ArrayList<String>();
 
         for (Resource resource : resources) {
+        	if (resource.getDirectory() == null) {
+        		// use default directory if not set.
+        		resource.setDirectory(defaultDirectory);
+        	}
+        	
+        	if (resource.getIncludes().isEmpty()) {
+        		// use default file pattern if not set.
+        		resource.getIncludes().add(defaultFilePattern);
+        	}
             // Build Scanner
-            DirectoryScanner scanner = buildScanner(resource);
+            DirectoryScanner scanner = buildScanner(baseDir.getPath(),resource);
             scanner.scan();
             for (String basename : scanner.getIncludedFiles()) {
                 founds.add(baseDir.toURI().relativize(new File(scanner.getBasedir(), basename).toURI()).getPath());
@@ -51,16 +60,16 @@ public class SQLScannerHelper {
      * @param resource
      * @return
      */
-    private static DirectoryScanner buildScanner(Resource resource) {
+    private static DirectoryScanner buildScanner(String baseDir,Resource resource) {
         if (resource != null) {
-            File baseDir = new File(resource.getDirectory());
-            if (!baseDir.exists() || !baseDir.isDirectory() || !baseDir.canRead()) {
+            File fileBaseDir = new File(baseDir,resource.getDirectory());
+            if (!fileBaseDir.exists() || !fileBaseDir.isDirectory() || !fileBaseDir.canRead()) {
                 throw new IllegalArgumentException(
                         format("Invalid <directory> %s in resource. Check your pom.xml", resource.getDirectory()));
             }
 
             DirectoryScanner scanner = new DirectoryScanner();
-            scanner.setBasedir(resource.getDirectory());
+            scanner.setBasedir(fileBaseDir.getPath());
             scanner.setIncludes(resource.getIncludes().toArray(new String[0]));
             scanner.setExcludes(resource.getExcludes().toArray(new String[0]));
             return scanner;
