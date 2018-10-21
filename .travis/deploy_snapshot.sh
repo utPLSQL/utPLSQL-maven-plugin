2a0db26265376c31adfecd0a228a410333311756
Vinicius Avellar <amvinicius@gmail.com>
2018-10-21 12:54:53 +0100

    Removed unecessary snapshot tagging

--- .travis/deploy_snapshot.sh
+++ .travis/deploy_snapshot.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -ev
 
-DATE=`date +%Y%m%d%H%M`
-#using this solution to get version into variable: https://stackoverflow.com/a/26514030/1470603
-OLD_VERSION=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)
-VERSION=`sed "s/-SNAPSHOT/-${DATE}-SNAPSHOT/g" <<<"${OLD_VERSION}"`
+#DATE=`date +%Y%m%d%H%M`
+# Using this solution to get version into variable: https://stackoverflow.com/a/26514030/1470603
+#OLD_VERSION=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)
+#VERSION=`sed "s/-SNAPSHOT/-${DATE}-SNAPSHOT/g" <<<"${OLD_VERSION}"`
 
-echo "Changing snapshot version, OLD: ${OLD_VERSION} NEW: ${VERSION}"
-mvn org.codehaus.mojo:versions-maven-plugin:2.7:set -DnewVersion=${VERSION}
+#echo "Changing snapshot version, OLD: ${OLD_VERSION} NEW: ${VERSION}"
+#mvn org.codehaus.mojo:versions-maven-plugin:2.7:set -DnewVersion=${VERSION}
 
 cd utplsql-maven-plugin
 mvn deploy -DskipTests -U -Prelease
