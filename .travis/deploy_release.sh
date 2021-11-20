240b2333452a66e80e38eaf9011d462093c2a6ce
Simon Martinelli <simon@72.services>
2021-11-20 12:19:59 +0100

    Cleaned Maven scripts

--- .travis/deploy_release.sh
+++ .travis/deploy_release.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 set -ev
 
-VERSION=$(tr -d "/v/" <<<${TRAVIS_TAG})
-mvn org.codehaus.mojo:versions-maven-plugin:2.7:set -DnewVersion=${VERSION}
-
 mvn deploy -DskipTests -U -Prelease
--- .travis/deploy_snapshot.sh
+++ .travis/deploy_snapshot.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 set -ev
 
-#DATE=`date +%Y%m%d%H%M`
-# Using this solution to get version into variable: https://stackoverflow.com/a/26514030/1470603
-#OLD_VERSION=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)
-#VERSION=`sed "s/-SNAPSHOT/-${DATE}-SNAPSHOT/g" <<<"${OLD_VERSION}"`
-
-#echo "Changing snapshot version, OLD: ${OLD_VERSION} NEW: ${VERSION}"
-#mvn org.codehaus.mojo:versions-maven-plugin:2.7:set -DnewVersion=${VERSION}
-
 mvn deploy -DskipTests -U -Prelease
