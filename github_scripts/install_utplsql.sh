f790e6b8187d9702ff3065c56727ec52c58fb8f9
Simon Martinelli <simon@72.services>
2021-11-23 08:16:16 +0100

    Fixed sqlplus docker call

--- github_scripts/install_utplsql.sh
+++ github_scripts/install_utplsql.sh
@@ -4,4 +4,4 @@ curl -Lk "${UTPLSQL_DOWNLOAD_URL}" -o utPLSQL.zip
 
 unzip -q utPLSQL.zip
 
-docker run --rm -v /home/runner/work/utPLSQL-maven-plugin/utPLSQL-maven-plugin/utPLSQL/source:/utPLSQL -w /utPLSQL/source --network host --entrypoint sqlplus truemark/sqlplus sys/oracle@//127.0.0.1:1521/XE as sysdba @install_headless.sql UT3 UT3 users
+docker run --rm -v /home/runner/work/utPLSQL-maven-plugin/utPLSQL-maven-plugin/utPLSQL:/utPLSQL -w /utPLSQL/source --network host --entrypoint sqlplus truemark/sqlplus sys/oracle@//127.0.0.1:1521/XE as sysdba @install_headless.sql UT3 UT3 users
