6a3edab0522519481f6d3fae47c4d1d9f8589b5d
Simon Martinelli <simon@72.services>
2021-11-23 08:26:36 +0100

    Added install_demo_project.sh

--- github_scripts/install_demo_project.sh
+++ github_scripts/install_demo_project.sh
@@ -1,11 +1,11 @@
 docker run --rm -v /home/runner/work/utPLSQL-maven-plugin/utPLSQL-maven-plugin/github_scripts:/project -w /project --network host --entrypoint sqlplus truemark/sqlplus \
-    sys/oracle@//127.0.0.1:1521/XE as sysdba @.travis/sql/create_users.sql
+    sys/oracle@//127.0.0.1:1521/XE as sysdba @github_scripts/sql/create_users.sql
 
 docker run --rm -v /home/runner/work/utPLSQL-maven-plugin/utPLSQL-maven-plugin/github_scripts:/project -w /project --network host --entrypoint sqlplus truemark/sqlplus \
-    app/pass@//127.0.0.1:1521/XE @.travis/sql/create_app_objects.sql
+    app/pass@//127.0.0.1:1521/XE @github_scripts/sql/create_app_objects.sql
 
 docker run --rm -v /home/runner/work/utPLSQL-maven-plugin/utPLSQL-maven-plugin/github_scripts:/project -w /project --network host --entrypoint sqlplus truemark/sqlplus \
-    code_owner/pass@//127.0.0.1:1521/XE @.travis/sql/create_source_owner_objects.sql
+    code_owner/pass@//127.0.0.1:1521/XE @github_scripts/sql/create_source_owner_objects.sql
 
 docker run --rm -v /home/runner/work/utPLSQL-maven-plugin/utPLSQL-maven-plugin/github_scripts:/project -w /project --network host --entrypoint sqlplus truemark/sqlplus \
-    tests_owner/pass@//127.0.0.1:1521/XE @.travis/sql/create_tests_owner_objects.sql
+    tests_owner/pass@//127.0.0.1:1521/XE @github_scripts/sql/create_tests_owner_objects.sql
