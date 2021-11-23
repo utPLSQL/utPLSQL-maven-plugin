ed38f0370e76dd2fe1e7d7d9ed374c273ce41fd0
Simon Martinelli <simon@72.services>
2021-11-23 08:35:56 +0100

    Fixed pathes

--- github_scripts/install_demo_project.sh
+++ github_scripts/install_demo_project.sh
@@ -1,15 +1,15 @@
-docker run --rm -v /home/runner/work/utPLSQL-maven-plugin/utPLSQL-maven-plugin/github_scripts:/github_scripts -w /github_scripts/sql \
+docker run --rm -v /home/runner/work/utPLSQL-maven-plugin/utPLSQL-maven-plugin:/work -w /work/ \
     --network host --entrypoint sqlplus truemark/sqlplus \
-    sys/oracle@//127.0.0.1:1521/XE as sysdba @create_users.sql
+    sys/oracle@//127.0.0.1:1521/XE as sysdba @github_scripts/sql/create_users.sql
 
-docker run --rm -v /home/runner/work/utPLSQL-maven-plugin/utPLSQL-maven-plugin/github_scripts:/github_scripts -w /github_scripts/sql \
+docker run --rm -v /home/runner/work/utPLSQL-maven-plugin/utPLSQL-maven-plugin:/work -w /work/ \
     --network host --entrypoint sqlplus truemark/sqlplus \
-    app/pass@//127.0.0.1:1521/XE @create_app_objects.sql
+    app/pass@//127.0.0.1:1521/XE @github_scripts/sql/create_app_objects.sql
 
-docker run --rm -v /home/runner/work/utPLSQL-maven-plugin/utPLSQL-maven-plugin/github_scripts:/github_scripts -w /github_scripts/sql \
+docker run --rm -v /home/runner/work/utPLSQL-maven-plugin/utPLSQL-maven-plugin:/work -w /work/ \
     --network host --entrypoint sqlplus truemark/sqlplus \
-    code_owner/pass@//127.0.0.1:1521/XE @create_source_owner_objects.sql
+    code_owner/pass@//127.0.0.1:1521/XE @github_scripts/sql/create_source_owner_objects.sql
 
-docker run --rm -v /home/runner/work/utPLSQL-maven-plugin/utPLSQL-maven-plugin/github_scripts:/github_scripts -w /github_scripts/sql \
+docker run --rm -v /home/runner/work/utPLSQL-maven-plugin/utPLSQL-maven-plugin:/work -w /work/ \
     --network host --entrypoint sqlplus truemark/sqlplus \
-    tests_owner/pass@//127.0.0.1:1521/XE @create_tests_owner_objects.sql
+    tests_owner/pass@//127.0.0.1:1521/XE @github_scripts/sql/create_tests_owner_objects.sql
