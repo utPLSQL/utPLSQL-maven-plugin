260603cb77c60c27db14699e17f73aa54fc4a578
Simon Martinelli <simon@72.services>
2021-11-24 11:55:58 +0100

    Back to IT

--- src/test/java/org/utplsql/maven/plugin/UtPLSQLMojoIT.java
+++ src/test/java/org/utplsql/maven/plugin/UtPLSQLMojoIT.java
@@ -4,7 +4,7 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
 import org.junit.Test;
 
-public class UtPLSQLMojoTest {
+public class UtPLSQLMojoIT {
 
     @Test
     public void execute() throws MojoExecutionException {
