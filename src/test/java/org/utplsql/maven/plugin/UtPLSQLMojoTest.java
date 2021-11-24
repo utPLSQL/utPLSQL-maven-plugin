e13c1925a16a046e1f8534a8b96cde1c5c192266
Simon Martinelli <simon@72.services>
2021-11-24 13:09:40 +0100

    Added assertion

--- src/test/java/org/utplsql/maven/plugin/UtPLSQLMojoTest.java
+++ src/test/java/org/utplsql/maven/plugin/UtPLSQLMojoTest.java
@@ -2,22 +2,32 @@ package org.utplsql.maven.plugin;
 
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.utplsql.api.reporter.CoreReporters;
 import org.utplsql.maven.plugin.model.ReporterParameter;
 
+import java.io.File;
+
+import static org.junit.Assert.assertTrue;
+
 public class UtPLSQLMojoTest {
 
-    @Test
-    public void execute() throws MojoExecutionException {
-        UtPLSQLMojo utPLSQLMojo = new UtPLSQLMojo();
+    private static UtPLSQLMojo utPLSQLMojo;
+
+    @BeforeClass
+    public static void setUp() {
+        utPLSQLMojo = new UtPLSQLMojo();
         utPLSQLMojo.project = new MavenProject();
         utPLSQLMojo.targetDir = "target";
 
         utPLSQLMojo.url = "jdbc:oracle:thin:@127.0.0.1:1521:XE";
         utPLSQLMojo.user = "UT3";
         utPLSQLMojo.password = "UT3";
+    }
 
+    @Test
+    public void junitReporter() throws MojoExecutionException {
         ReporterParameter junitReporter = new ReporterParameter();
         junitReporter.setConsoleOutput(true);
         junitReporter.setFileOutput("junit-report.xml");
@@ -25,5 +35,7 @@ public class UtPLSQLMojoTest {
         utPLSQLMojo.reporters.add(junitReporter);
 
         utPLSQLMojo.execute();
+
+        assertTrue(new File("target/junit-report.xml").exists());
     }
 }
