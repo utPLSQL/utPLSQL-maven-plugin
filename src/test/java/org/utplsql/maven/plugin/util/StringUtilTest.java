a6b4e4d692d74b855872db6fb7028ab02eda0f97
Simon Martinelli <simon@72.services>
2021-12-03 15:51:12 +0100

    Added false tests

--- src/test/java/org/utplsql/maven/plugin/util/StringUtilTest.java
+++ src/test/java/org/utplsql/maven/plugin/util/StringUtilTest.java
@@ -2,6 +2,7 @@ package org.utplsql.maven.plugin.util;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.utplsql.maven.plugin.util.StringUtil.isBlank;
 import static org.utplsql.maven.plugin.util.StringUtil.isEmpty;
@@ -11,22 +12,42 @@ import static org.utplsql.maven.plugin.util.StringUtil.isNotEmpty;
 class StringUtilTest {
 
     @Test
-    void is_empty() {
+    void is_empty_true() {
         assertTrue(isEmpty(""));
     }
 
     @Test
-    void is_not_empty() {
+    void is_empty_false() {
+        assertFalse(isEmpty("abc"));
+    }
+
+    @Test
+    void is_not_empty_true() {
         assertTrue(isNotEmpty("abc"));
     }
 
     @Test
-    void is_blank() {
+    void is_not_empty_false() {
+        assertFalse(isNotEmpty(""));
+    }
+
+    @Test
+    void is_blank_true() {
         assertTrue(isBlank(" "));
     }
 
     @Test
-    void is_not_blank() {
+    void is_blank_false() {
+        assertFalse(isBlank("abc"));
+    }
+
+    @Test
+    void is_not_blank_true() {
         assertTrue(isNotBlank("abc"));
+    }
+
+    @Test
+    void is_not_blank_false() {
+        assertFalse(isNotBlank(""));
     }
 }
