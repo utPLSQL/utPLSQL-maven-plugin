7d4596d5dd30f16cf89ef59cf9067ae863cce956
Simon Martinelli <simon@72.services>
2021-12-09 20:09:23 +0100

    Merged if statements

--- src/main/java/org/utplsql/maven/plugin/util/StringUtil.java
+++ src/main/java/org/utplsql/maven/plugin/util/StringUtil.java
@@ -6,13 +6,7 @@ public class StringUtil {
     }
 
     public static boolean isEmpty(CharSequence cs) {
-        if (cs == null) {
-            return true;
-        }
-        if (cs.length() == 0) {
-            return true;
-        }
-        return false;
+        return cs == null || cs.length() == 0;
     }
 
     public static boolean isNotEmpty(CharSequence cs) {
@@ -20,13 +14,10 @@ public class StringUtil {
     }
 
     public static boolean isBlank(CharSequence cs) {
-        int strLen;
-        if (cs != null) {
-            if ((strLen = cs.length()) != 0) {
-                for (int i = 0; i < strLen; ++i) {
-                    if (!Character.isWhitespace(cs.charAt(i))) {
-                        return false;
-                    }
+        if (cs != null && cs.length() > 0) {
+            for (int i = 0; i < cs.length(); ++i) {
+                if (!Character.isWhitespace(cs.charAt(i))) {
+                    return false;
                 }
             }
         }
