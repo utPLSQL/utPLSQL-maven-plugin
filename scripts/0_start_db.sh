36d6ab3c6a5094d9b0a790e008214da32e7bfa87
Simon Martinelli <simon@martinelli.ch>
2024-08-31 14:00:24 +0200

    Upgrade to Oracle Database Free #74

--- scripts/0_start_db.sh
+++ scripts/0_start_db.sh
@@ -1,1 +1,1 @@
-docker run -d --name ora-utplsql -p 1521:1521 -e ORACLE_PASSWORD=oracle gvenzl/oracle-xe:21-slim
+docker run -d --name ora-utplsql -p 1521:1521 -e ORACLE_PASSWORD=oracle gvenzl/oracle-free:23-slim
