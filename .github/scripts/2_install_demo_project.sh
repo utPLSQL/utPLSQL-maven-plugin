f601046c0fa7317b7ef0b1ff9d9dc36dc6964dda
Jacek Gębal <jacek.gebal@smenso.de>
2026-04-25 21:31:14 +0300

    Update build process.

--- .github/scripts/2_install_demo_project.sh
+++ .github/scripts/2_install_demo_project.sh
@@ -1,11 +1,12 @@
+#!/bin/bash
 docker run --rm -v $(pwd):/work -w /work/  --network host --entrypoint sqlplus truemark/sqlplus:19.8 \
-    sys/oracle@${DB_URL} as sysdba @scripts/sql/create_users.sql
+    sys/oracle@${DB_URL} as sysdba @.github/scripts/sql/create_users.sql
 
 docker run --rm -v $(pwd):/work -w /work/  --network host --entrypoint sqlplus truemark/sqlplus:19.8 \
-    app/pass@${DB_URL} @scripts/sql/create_app_objects.sql
+    app/pass@${DB_URL} @.github/scripts/sql/create_app_objects.sql
 
 docker run --rm -v $(pwd):/work -w /work/  --network host --entrypoint sqlplus truemark/sqlplus:19.8 \
-    code_owner/pass@${DB_URL} @scripts/sql/create_source_owner_objects.sql
+    code_owner/pass@${DB_URL} @.github/scripts/sql/create_source_owner_objects.sql
 
 docker run --rm -v $(pwd):/work -w /work/  --network host --entrypoint sqlplus truemark/sqlplus:19.8 \
-    tests_owner/pass@${DB_URL} @scripts/sql/create_tests_owner_objects.sql
+    tests_owner/pass@${DB_URL} @.github/scripts/sql/create_tests_owner_objects.sql
